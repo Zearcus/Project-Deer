b67fadf6096d38dba9e5ae63e0b70535f7280609
Fabien Marchand <ghost.fm65@gmail.com>
2023-06-14 17:58:02 +0200

    first creation of database
    Create json

---
+++ Assets/Scripts/JsonTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class JsonTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        File.write
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
