15933b380015c01d9dfa86e1f22b1143a8a423b5
Clément LI <cli@gaming.tech>
2023-06-15 17:39:56 +0200

    Avancement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,31 +7,42 @@ public class PlayerMovement : MonoBehaviour
     GameObject myCanvas;
     public List<GameObject> waypoints;
     public float speed = 2f;
+    int index = 0;
 
     private void Start() 
     {
         enabled = false;
     }
-    IEnumerator Move()
+    /*IEnumerator Move()
     {
-        int index = 0;
         Vector3 destination = waypoints[index].transform.position;
         Vector3 newPos = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
         transform.position = newPos;
+        index++;
         yield return new WaitForFixedUpdate();
-    }
+    }*/
 
     private void Update() 
     {
         if (enabled == true)
         {
-            StartCoroutine(Move());
-
-            foreach (Transform element in GameObject.FindWithTag("Canvas").transform)
+            Vector3 destination = waypoints[index].transform.position;
+            Vector3 newPos = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            transform.position = newPos;
+            
+            if (index < waypoints.Count-1)
             {
-                element.gameObject.SetActive(false);
+                index++;
             }
-              
         }
+    }
+
+    private void OnTriggerStay(Collider col) 
+    {
+        if (gameObject.transform.position == col.transform.position)
+        {
+            speed = 0f;
+        }
+        Debug.Log(col);
     }
 }
--- Assets/Scripts/ChoiceEvent.cs
+++ Assets/Scripts/ChoiceEvent.cs
@@ -4,17 +4,20 @@ using UnityEngine.SceneManagement;
 public class ChoiceEvent : MonoBehaviour, IClicked
 {
     GameObject myCanvas;
+    public GameObject Narration;
+    int index;
 
     private void Awake() 
     {
         myCanvas = GameObject.FindWithTag("Canvas");
+        index = 0;
     }
 
     public void onClickedEvent()
     {
         foreach (Transform element in GameObject.FindWithTag("Canvas").transform)
-        {
-            element.gameObject.SetActive(true);
+        {           
+            Narration.SetActive(true);
         }
     }
 }
