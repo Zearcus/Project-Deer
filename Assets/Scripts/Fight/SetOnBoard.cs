11ceb63e23461ed932039dbcbfb9f9b9d1ac4819
Fabien Marchand <ghost.fm65@gmail.com>
2023-07-12 21:12:30 +0200

    optimize code

--- Assets/Scripts/Fight/SetOnBoard.cs
+++ Assets/Scripts/Fight/SetOnBoard.cs
@@ -4,43 +4,17 @@ using UnityEngine;
 
 public class SetOnBoard : MonoBehaviour
 {
-    
-    LoadCards load;
+    MouseClickManager mouse;
     public GameObject Area;
-    private string CheckTypeCards;
-
-    private int MaxSpawnArea = 0, CurrentValue = 0;
+    private int MaxSpawnArea = 4, CurrentValue = 0;
     private float PosZ;
 
     //create a a collider area for set cards in the board
     public void AreaSetCards(GameObject game)
     {
-        load = GameObject.Find("Card").GetComponent<LoadCards>();
         if (game.tag == "Cards")
         {
-            if (load.textType.text == "Weapon")
-            {
-                CheckTypeCards = load.textType.text;
-                SetTypeForColliders();
-                CreateColliders();
-            }
-        }
-    }
-
-    private void SetTypeForColliders()
-    {
-        // Check the type if the card and set the number of spawning colliders with cards types
-        switch (CheckTypeCards)
-        {
-            case "Weapon":
-                MaxSpawnArea = 1;
-                break;
-            case "Spell":
-                MaxSpawnArea = 2;
-                break;
-            case "Utilitie":
-                MaxSpawnArea = 3;
-                break;
+            CreateColliders();
         }
     }
 
@@ -49,9 +23,8 @@ public class SetOnBoard : MonoBehaviour
         // create colliders
         while (CurrentValue != MaxSpawnArea)
         {
-            Area = Instantiate(Area, new Vector3(-1.5f, 0.3f, 3.0f + PosZ), transform.rotation);
-            Area.name = CheckTypeCards;
-            Area.tag = "Weapon";
+            Area = Instantiate(Area, new Vector3(-1.5f, 0.3f, 2.0f + PosZ), transform.rotation);
+            Area.name = "collider";
             CurrentValue++;
             PosZ = PosZ - 1.0f;
         }
