a7d7517dc8ceb713a486ff0338e63d0ba7369035
Fabien Marchand <ghost.fm65@gmail.com>
2023-07-25 15:22:54 +0200

    new database with json for dialogues

--- Assets/Scripts/Database/Database.cs
+++ Assets/Scripts/Database/Database.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
     
-public class Database{
+public class Database : MonoBehaviour{
+
+    public DialogueDatabase dialog = new DialogueDatabase();
+    
     public string NameCard = "";
-    public string NameDialogue = "";
     public string NameEnemies = "";
+    public int IDDialogue;
 
 
     public Dictionary<string, CardData> Card = new Dictionary<string, CardData>(){
@@ -17,12 +21,15 @@ public class Database{
         }
     };
 
-    public Dictionary<string, Dialogue> Dialogue = new Dictionary<string, Dialogue>(){
-        {"part1 dialog 1",new Dialogue{
-            NameCharacter = "Samten",
-            CoreText = "Ceci est le premier dialogue du jeu"} 
-        }
-    };
+    public void LoadFromJson()
+    {
+
+        string filePath = "Assets/Scripts/Database/Dialogue.json";
+        string DialogueData = System.IO.File.ReadAllText(filePath);
+
+        dialog = JsonUtility.FromJson<DialogueDatabase>(DialogueData);
+        Debug.Log("ouai ça marche de ouf");
+    }
 
     public Dictionary<string, EnemyData> Enemies = new Dictionary<string, EnemyData>(){
 
@@ -50,14 +57,25 @@ public class EnemyData{
 
 }
 
-public class Dialogue{
-   public string NameCharacter;
-   public string CoreText;
-   public string Picture;
+//Dialogue Part
+[System.Serializable]
+public class DialogueDatabase
+{
+    public List<Dialogue> dialogues = new List<Dialogue>();
+}
+[System.Serializable]
+public class Dialogue
+{
+    public string NameCharacter;
+    public string CoreText;
+    public string Picture;
 }
 
 /*
     Call example:
-    Dialogue[NameDialogue].NameCharacter; calling the name with key value
-    Dialogue[NameDialogue].CoreText; calling the text with key value
+        - Dialogue[NameDialogue].NameCharacter; calling the name with key value
+        - Dialogue[NameDialogue].CoreText; calling the text with key value
+
+    call dialogue:
+        - dialog.dialogues[IDDialogue].NameCharacter;
 */
