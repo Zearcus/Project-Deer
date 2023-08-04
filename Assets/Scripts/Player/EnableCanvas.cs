5a62063b1ed8f071cf56f313f5263191c604285a
Clément LI <cli@gaming.tech>
2023-08-04 17:38:31 +0200

    Test

--- Assets/Scripts/Player/EnableCanvas.cs
+++ Assets/Scripts/Player/EnableCanvas.cs
@@ -37,26 +37,56 @@ public class EnableCanvas : MonoBehaviour
     
     private IEnumerator NextDialogue(DialogueObject dialogueObject)
     {
+        dialogueText.ForceMeshUpdate();
+
+        bool displaying = true;
+        bool progressing = true;
+    
         for (int i = 0; i < dialogueObject.dialogueLines.Length; i++)
         {
             nameText.text = dialogueObject.dialogueLines[i].name;
             dialogueText.text = dialogueObject.dialogueLines[i].dialogue;
             _narrationObject.sprite = dialogueObject.dialogueLines[i].narBG;
+
             dialogueText.text = "";
-            
+
             foreach(char c in dialogueObject.dialogueLines[i].dialogue.ToCharArray())
             {
-                dialogueText.text += c;
-                yield return new WaitForSeconds(textSpeed);
+                if (progressing)
+                {
+                    dialogueText.text += c;
+                    yield return new WaitForSeconds(textSpeed);
+
+                    if(dialogueText.text.Length == dialogueObject.dialogueLines[i].dialogue.Length)
+                    {
+                        progressing = false;
+                        displaying = false;
+                    }
+                }
+
+                if (Input.GetMouseButton(0) && displaying)
+                {
+                    dialogueText.text = "";
+                    displaying = false;
+                    progressing = false;
+                    Debug.Log("Debug 1 =" + displaying);
+                }   
             }
 
-            yield return new WaitUntil(()=> Input.GetMouseButtonDown(0));
-            yield return null;
+            if (!displaying)
+            {
+                yield return new WaitUntil(()=>Input.GetMouseButtonDown(0));
+                yield return null;
+                displaying = true;
+                progressing = true;
+                Debug.Log("Debug 2 =" + displaying);
+            }
         }
-
+        
         Narration.SetActive(false);
         _player.canMove = true;
         point++;
+        StopAllCoroutines();
     }
 
     public void DisplayDialogue(DialogueObject dialogueObject)
